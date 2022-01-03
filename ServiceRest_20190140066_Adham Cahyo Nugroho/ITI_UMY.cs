53de4bf11f70d79e7bb14c3dc8796054aa94905e
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2022-01-03 18:40:36 +0700

    Initial commit

---
+++ ServiceRest_20190140066_Adham Cahyo Nugroho/ITI_UMY.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using static ServiceRest_20190140066_Adham_Cahyo_Nugroho.TI_UMY;
+
+namespace ServiceRest_20190140066_Adham_Cahyo_Nugroho
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        [DataContract]
+        public class Mahasiswa
+        {
+            private string _nama, _nim, _prodi, _angkatan; //adalah konversi atau kesepakatan //variabel lokal
+            [DataMember(Order = 1)] // mengirim data utk mengurutkan //
+            public string nama
+            {
+                get { return _nama; }
+                set { _nama = value; }
+            }
+            [DataMember(Order = 2)]
+            public string nim
+            {
+                get { return _nim; }
+                set { _nim = value; }
+            }
+            [DataMember(Order = 3)]
+            public string prodi
+            {
+                get { return _prodi; }
+                set { _prodi = value; }
+            }
+            [DataMember(Order = 4)]
+            public string angkatan
+            {
+                get { return _angkatan; }
+                set { _angkatan = value; }
+            }
+        }
+
+        // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+        [ServiceContract]
+        public interface ITI_UMY
+        {
+            [OperationContract]
+            [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] //utk membuat slash, selalu relative
+            List<Mahasiswa> GetAllMahasiswa(); //mendapatkan kumpulan mahasiswa/seluruh datamahasiswa
+
+            [OperationContract] /*nama method*/
+            [WebGet(UriTemplate = "Mahasiswa/nim={nim}", ResponseFormat = WebMessageFormat.Json)] //untuk get
+            Mahasiswa GetMahasiswaByNIM(string nim); //mengambil data berdasarkan nim
+
+            //void CreateMahasiswa(Mahasiswa mhs); tidak ada pengembalian atau respon
+
+            [OperationContract]
+            [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+            string CreateMahasiswa(Mahasiswa mhs);
+
+            // TODO: Add your service operations here
+        }
+
+        // Use a data contract as illustrated in the sample below to add composite types to service operations.
+        // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "_ServiceRest_20190140066_Adham Cahyo.ContractType".
+        [DataContract]
+        public class CompositeType
+        {
+            bool boolValue = true;
+            string stringValue = "Hello ";
+
+            [DataMember]
+            public bool BoolValue
+            {
+                get { return boolValue; }
+                set { boolValue = value; }
+            }
+
+            [DataMember]
+            public string StringValue
+            {
+                get { return stringValue; }
+                set { stringValue = value; }
+            }
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class WebGetAttribute : Attribute
+    {
+        public object ResponseFormat { get; set; }
+        public string UriTemplate { get; set; }
+    }
+
+    internal class WebMessageFormat
+    {
+    }
+
+    internal class WebInvokeAttribute : Attribute
+    {
+        public object RequestFormat;
+
+        public string UriTemplate { get; set; }
+        public string Method { get; set; }
+        public object ResponseFormat { get; set; }
+    }
+}
---
+++ ServiceRest_20190140066_Adham Cahyo Nugroho/TI_UMY.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using static ServiceRest_20190140066_Adham_Cahyo_Nugroho.TI_UMY;
+
+namespace ServiceRest_20190140066_Adham_Cahyo_Nugroho
+{
+    public string CreateMahasiswa(Mahasiswa mhs)
+    {
+        string msg = "GAGAL";
+        SqlConnection sqlcon = new SqlConnection("Data Source=E3-SENTRY;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bariskartalii737aewc");
+        string query = String.Format("insert into dbo.Mahasiswa values ('{0}', '{1}','{2}','{3}')", mhs.nama, mhs.nim, mhs.prodi, mhs.angkatan);
+        //NIM = '{0}'", nim
+        //string query = "insert into dbo.Mahasiswa values {'"+mhs.nama+"', '"+mhs.nim+"', '"+'"+mhs.prodi+"', '"+mhs.angkatan+"')";
+        SqlCommand sqlcom = new SqlCommand(query, sqlcon); //yg dikirim ke sql
+
+        try
+        {
+            sqlcon.Open();
+            Console.WriteLine(query);
+            sqlcom.ExecuteNonQuery();
+            sqlcon.Close();
+            msg = "Sukses";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(query);
+            msg = "GAGAL";
+        }
+        return msg;
+    }
+
+    public List<Mahasiswa> GetAllMahasiswa()
+    {
+        List<Mahasiswa> mahas = new List<Mahasiswa>();
+
+        SqlConnection con = new SqlConnection("Data Source=E3-SENTRY;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bariskartalii737aewc");
+        string query = "select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa";
+        SqlCommand com = new SqlCommand(query, con); //yg dikirim ke sql
+
+        try
+        {
+            con.Open();
+            SqlDataReader reader = com.ExecuteReader(); //mendapatkan data yg telah dieksekusi, dari select, hasil query ditaruh di reader
+
+            while (reader.Read())
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.nama = reader.GetString(0); //) itu array pertama // in diambil dari IService
+                mhs.nim = reader.GetString(1);
+                mhs.prodi = reader.GetString(2);
+                mhs.angkatan = reader.GetString(3);
+
+                mahas.Add(mhs);
+            }
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(query);
+        }
+        return mahas; //output      
+    }
+
+    public string GetData(int value)
+    {
+        return string.Format("You entered: {0}", value);
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        if (composite == null)
+        {
+            throw new ArgumentNullException("composite");
+        }
+        if (composite.BoolValue)
+        {
+            composite.StringValue += "Suffix";
+        }
+        return composite;
+    }
+    public Mahasiswa GetMahasiswaByNIM(string nim)
+    {
+        Mahasiswa mhs = new Mahasiswa();
+        SqlConnection con = new SqlConnection("Data Source=E3-SENTRY;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bariskartalii737aewc");
+        string query = String.Format("select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+        SqlCommand com = new SqlCommand(query, con);
+
+        try
+        {
+            con.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                mhs.nama = reader.GetString(0); //) e itu array pertama // ni diambil dar iservice
+                mhs.nim = reader.GetString(1);
+                mhs.prodi = reader.GetString(2);
+                mhs.angkatan = reader.GetString(3);
+            }
+            con.Close();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(query);
+        }
+        return mhs;
+    }
+
+    internal class SqlDataReader
+    {
+        internal string GetString(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class SqlCommand
+    {
+        private string query;
+        private SqlConnection con;
+
+        public SqlCommand(string query, SqlConnection con)
+        {
+            this.query = query;
+            this.con = con;
+        }
+
+        internal void ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal SqlDataReader ExecuteReader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class SqlConnection
+    {
+        private string v;
+
+        public SqlConnection(string v)
+        {
+            this.v = v;
+        }
+
+        internal void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
